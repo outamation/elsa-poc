ce7c04da7388811aa1ee15d3defb23993ecf9f27
Santosh Satalkar <santosh.satalkar@outamation.com>
2022-11-03 19:36:27 +0530

    Adding Angular Code

---
+++ ng-src/src/app/app.component.ts
@@ -0,0 +1,191 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { MenuItem } from 'primeng/api';
+
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  workflowURL = "https://localhost:7190";
+  workflowStart = "/cms/fcfha"
+  worflowInstanceId = ""
+  worflowProgressUrl = this.workflowURL + "/workflow-instances"
+  title = 'Workflow';
+  menuItems: MenuItem[] = [];
+  stepItems: MenuItem[] = [];
+  caseStepTasks: any = [];
+  activeIndex = 0
+  showSideBar = false;
+  case: any = {
+    File: "Test-File-XYZ",
+    FileType: "Default Servicing",
+    InvType: "FHA",
+    Loan: "123456789",
+    Case: "9876543",
+    CaseType: "Foreclosure",
+    User: "Hawkins, Sam",
+    Atty: "Hawkins, Sam",
+    Address: "12529 State Road 535. Ste 531",
+    City: "Orlando",
+    County: "Bay County",
+    State: "Florida",
+    Zip: "32836"
+  }
+
+  constructor(private primengConfig: PrimeNGConfig,
+    private messageService: MessageService,
+    private http: HttpClient) { }
+
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.loadStartupData();
+  }
+
+  loadStartupData() {
+    this.stepItems = [
+      { label: 'Create Case' },
+      { label: 'Case Steps' },
+      { label: 'Complete' }
+    ];
+
+    this.menuItems = [{
+      label: 'Workflow ',
+      items: [
+        { label: 'New Case', icon: 'pi pi-fw pi-plus' },
+        { label: 'Workflow', icon: 'pi pi-fw pi-sitemap', url: this.workflowURL + "/workflow-instances" }
+      ]
+    }, {
+      label: 'Administration',
+      items: [
+        { label: 'Configuration', icon: 'pi pi-fw pi-wrench' },
+        { label: 'Fees', icon: 'pi pi-fw pi-dollar' },
+        { label: 'Users', icon: 'pi pi-fw pi-users' }
+      ]
+    }, {
+      label: 'Accounting',
+      items: [
+        { label: 'Onboarding', icon: 'pi pi-fw pi-user-plus' },
+        { label: 'Cost Payables ', icon: 'pi pi-fw pi-paypal' },
+        { label: 'Deposits', icon: 'pi pi-fw pi-dollar' }, { label: 'Vendor Fees', icon: 'pi pi-fw pi-dollar' }
+      ]
+    },
+    {
+      label: 'Case Management',
+      items: [
+        { label: 'Case Report', icon: 'pi pi-fw pi-print' },
+        { label: 'Case Docs', icon: 'pi pi-fw pi-verified' },
+        { label: 'Messages', icon: 'pi pi-fw pi-inbox' },
+        { label: 'Notes', icon: 'pi pi-fw pi-file-edit' }
+      ]
+    }, {
+      label: 'Reporting ',
+      items: [
+        { label: 'Design', icon: 'pi pi-fw pi-palette' },
+        { label: 'Case Report', icon: 'pi pi-fw pi-slack' }
+      ]
+    },];
+
+    this.setCaseStepTasks();
+  }
+  setCaseStepTasks() {
+    this.caseStepTasks = [
+      { stepName: 'File Referred To Attorney', stepSignal: 'FileReferred', disabled: false },
+      { stepName: 'File Received By Attorney', stepSignal: 'FileReceived', disabled: false },
+      { stepName: 'FC Title Ordered', stepSignal: 'FCTitleOrdered', disabled: false },
+      { stepName: 'FC SCRA Eligibility Review', stepSignal: 'FCSCRAEligibilityReview', disabled: false },
+      { stepName: 'Title Report Received', stepSignal: 'TitleReportReceived', disabled: true },
+      { stepName: 'Preliminary Title Clear', stepSignal: 'PreliminaryTitleClear', disabled: true },
+      { stepName: 'Complaint Filed', stepSignal: 'ComplaintFiled', disabled: true },
+      // { stepName: 'HUD First Action Expires', stepSignal: 'HUDFirstActionExpires', disabled: true },
+    ];
+  }
+
+  reforecastStepTasks() {
+    this.httpGet(this.workflowStart);
+    this.setCaseStepTasks();
+  }
+  caseCreate() {
+    if (!this.worflowInstanceId) {
+      this.httpGet(this.workflowStart);
+      this.showSuccess("Case is created successfully, Please proceed with case steps.");
+    }
+    this.next();
+  }
+
+  caseStepsComplete() {
+    this.isWorkflowComplete();
+  }
+
+  caseComplete() {
+    this.showSuccess("The case is now completed.");
+  }
+
+  back() {
+    this.activeIndex = this.activeIndex - 1;
+  }
+  next() {
+    this.activeIndex = this.activeIndex + 1;
+  }
+
+  showSuccess(msg: any) {
+    this.messageService.clear();
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: msg });
+  }
+
+  showError(msg: any) {
+    this.messageService.clear();
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: msg });
+  }
+
+  httpGet(endpoint: any) {
+    this.http.get(this.workflowURL + endpoint, { responseType: 'text' }).subscribe(data => {
+      this.worflowInstanceId = data;
+      console.log(this.worflowInstanceId);
+      this.worflowProgressUrl = this.workflowURL + "/workflow-instances/" + this.worflowInstanceId;
+    })
+  }
+
+  isWorkflowComplete() {
+    this.http.get(this.workflowURL + "/v1/workflow-instances/" + this.worflowInstanceId, { responseType: 'text' })
+      .subscribe((data) => {
+        console.log(data);
+        // if(data.workflowStatus==="Finished"){
+        this.showSuccess("All the Case steps are Complete, the case can be marked complete.");
+        this.next();
+      })
+  }
+
+  httpPost(step: any) {
+    console.log(step);
+
+    this.http.post<any>(`${this.workflowURL}/v1/custom-signals/${step.stepSignal}/execute`, { workflowInstanceId: this.worflowInstanceId })
+      .subscribe(data => {
+        console.log('Post Response', data);
+        this.showSuccess(`Case step '${step.stepName}' is completed successfully.`);
+        this.caseStepTasks.map((x: { stepName: string; stepSignal: string; completed: boolean; disabled: boolean; }) => {
+          if (x.stepSignal === step.stepSignal) {
+            x.stepName = x.stepName + ' - Complete';
+            x.completed = true;
+          }
+          if (step.stepSignal === 'FCTitleOrdered') {
+            x.disabled = false;
+            if (x.stepSignal === 'FCSCRAEligibilityReview') {
+              x.disabled = true;
+            }
+          }
+          if (step.stepSignal === 'FCSCRAEligibilityReview') {
+            if (x.stepSignal === 'FCTitleOrdered') {
+              x.disabled = true;
+            }
+          }
+        });
+
+      })
+  }
+}
---
+++ ng-src/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StepsModule } from 'primeng/steps';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu';
+import { RippleModule } from 'primeng/ripple';
+import { HttpClientModule } from '@angular/common/http';
+import { AccordionModule } from 'primeng/accordion';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [CommonModule, BrowserModule, AppRoutingModule, BrowserAnimationsModule, ProgressSpinnerModule, FormsModule,
+    StepsModule, ToastModule, ButtonModule, CardModule, SidebarModule, MenuModule, RippleModule, AccordionModule,
+    HttpClientModule
+  ],
+  providers: [MessageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
